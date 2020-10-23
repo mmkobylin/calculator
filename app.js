3b21f4095c480f7b1e65b24aef9e8f268f253f70
mmkobylin <mmkobylin@gmail.com>
2020-10-23 14:44:52 +0100

    data types added, variables for js declared

---
+++ app.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll('[data-number')
+const operationButtons = document.querySelectorAll('[data-operation')
+const deleteButton = document.querySelector('[data-delete')
+const allClearButton = document.querySelector('[data-all-clear')
+const equalsButton = document.querySelector('[data-equal')
+const previousOperandTextElement = document.querySelector('[data-previous-operand')
+const currentOperandTextElement = document.querySelector('[data-current-operand')
