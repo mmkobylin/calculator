d35694c9f4849f848eab395f7a357510f00413c2
mmkobylin <mmkobylin@gmail.com>
2020-10-27 11:36:51 +0000

    operation symbol appended to the previousOperant display

--- app.js
+++ app.js
@@ -71,8 +71,10 @@ class Calculator {
 
     updateDisplay() {
         this.currentOperandTextElement.innerText = this.currentOperand;
-        this.previousOperandTextElement.innerText = this.previousOperand;
-
+        if (this.operation != null){
+            this.previousOperandTextElement.innerText =
+             `${this.previousOperand} ${this.operation}`
+        }
     }
 
 }
